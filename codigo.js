84e4d74860083b457a726088e25cf9137178091e
luismanti <luisenrique.mantillasarria@gmail.com>
2022-10-10 15:04:41 -0500

    produc detail aside info was added to the main html file, some css bugs was resolved, like same class names.

--- codigo.js
+++ codigo.js
@@ -5,27 +5,27 @@ const cardsContainer = document.querySelector('.cards-container');
 
 const desktopMenu = document.querySelector('.desktop-menu');
 const mobileMenu = document.querySelector('.mobile-menu');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContainer');
 
 navEmail.addEventListener('click', toogleDesktopMenu);
 iconMobileMenu.addEventListener('click', toogleMobileMenu);
 shoppingCart.addEventListener('click', toogleAsideCart);
 
 function toogleDesktopMenu(){
-    const isAsideCLosed = aside.classList.contains('inactive');
+    const isAsideCLosed = shoppingCartContainer.classList.contains('inactive');
     
     if (!isAsideCLosed) {
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     
     desktopMenu.classList.toggle('inactive');
 }
 
 function toogleMobileMenu(){
-    const isAsideCLosed = aside.classList.contains('inactive');
+    const isAsideCLosed = shoppingCartContainer.classList.contains('inactive');
     
     if (!isAsideCLosed) {
-        aside.classList.add('inactive')
+        shoppingCartContainer.classList.add('inactive')
     }
     
     mobileMenu.classList.toggle('inactive');
@@ -44,7 +44,7 @@ function  toogleAsideCart(){
         desktopMenu.classList.add('inactive')
     }
     
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 function renderList (arr){
