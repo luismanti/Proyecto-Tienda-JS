6a4ccc24b95aa092da070a22117df9315168b809
luismanti <luisenrique.mantillasarria@gmail.com>
2022-10-10 10:15:19 -0500

    The mobile and desktop navbar was implemented and integred in index.html file, the code is responsive with 725px media querys, toogle help us to apear and disapear some icons and items bweteen the desktop and mobile

--- codigo.js
+++ codigo.js
@@ -1,7 +1,17 @@
 const navEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu')
+const iconMobileMenu = document.querySelector('.menuHamIcon');
+
+const desktopMenu = document.querySelector('.desktop-menu');
+const mobileMenu = document.querySelector('.mobile-menu');
+
+
 navEmail.addEventListener('click', toogleDesktopMenu);
+iconMobileMenu.addEventListener('click', toogleMobileMenu);
 
 function toogleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
+}
+
+function toogleMobileMenu(){
+    mobileMenu.classList.toggle('inactive');
 }
